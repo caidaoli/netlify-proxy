8e26216aca9b73297916aa3fa4e788c2a1739681
caidaoli <caidaoli@qq.com>
2025-06-11 16:20:30 +0800

    Update proxy-handler.ts

--- netlify/edge-functions/proxy-handler.ts
+++ netlify/edge-functions/proxy-handler.ts
@@ -28,7 +28,8 @@ const PROXY_CONFIG = {
   "/tv": "https://tv.gally.ddns-ip.net",
   "/news": "https://newsnow-ahm.pages.dev",
   "/sophnet":"https://sophnet.com",
-  "/grok":"https://grok.com"
+  "/grok":"https://grok.com",
+  "/cerebras":"https://api.cerebras.ai"
 };
 
 // 需要修复路径的内容类型
